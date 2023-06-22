6061ac057f9a352c29fdb36ec96e9d34088df22d
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-06-23 01:05:08 +0200

    makie plot works

--- test_sync.jl
+++ test_sync.jl
@@ -5,6 +5,7 @@ Pkg.instantiate();
 import julia_cf2
 using PyCall
 using Printf
+using DataStructures
 
 # package imports
 cflib = pyimport("cflib")
@@ -21,7 +22,25 @@ uri = "usb://0"
 # Only output errors from the logging framework
 logging.basicConfig(level=logging.ERROR)
 
+# create circular buffer
+buffer_len = 5
 cb = CircularBuffer{Array{Float64,2}}(buffer_len)
+
+##
+
+# for glmakie rad_per_sample_to_hz
+points = Observable(Point2f[])
+
+# create GLMakie plot
+f = Figure()
+ax = Axis(f[1, 1])
+lines!(ax, points)
+limits!(ax, 0, 10, -4, 4)
+
+display(f)
+
+
+##
 
 function simple_log(scf, logconf)
 
@@ -47,9 +66,14 @@ function simple_log(scf, logconf)
             sample = [data["gyro_unfiltered.x"] data["gyro_unfiltered.y"] data["gyro_unfiltered.z"]]
             push!(cb, sample)
 
+            # push data to plot buffer
+            if count % 10 == 0
+                points[] = push!(points[], [(count / 1000) data["gyro_unfiltered.x"]])
+            end
+
             count = count + 1
 
-            if count == 100
+            if count == 10000
                 break
             end
         end
