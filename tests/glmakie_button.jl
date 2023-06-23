2bfcd272569907e38e306604de0ef25b248fba49
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-06-23 17:57:33 +0200

    button to stop async works

---
+++ tests/glmakie_button.jl
@@ -0,0 +1,41 @@
+using GLMakie
+GLMakie.activate!(inline=false)
+
+fig = Figure()
+
+ax = Axis(fig[1, 1])
+fig[2, 1] = buttongrid = GridLayout(tellwidth=false)
+
+display(fig)
+
+
+button = Button(fig, label="press")
+
+buttongrid[1, 1] = button
+
+flag = Observable(false)
+
+##
+on(button.clicks) do n
+    if flag[] == true
+        schedule(task1, :STOP, error=true)
+    end
+    # notify(counts)
+end
+
+##
+flag[] = 0
+
+
+task1 = @async try
+    flag[] = true
+    for i in 1:20
+        println(flag)
+        sleep(0.5)
+    end
+
+catch
+    flag[] = false
+    println("Interrupted!")
+end
+
