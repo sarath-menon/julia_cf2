8f12c7bc1d32030771b18a4d4438eeae078bda07
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-06-23 08:43:26 +0200

    disabled interactivity in plot

--- plot_async.jl
+++ plot_async.jl
@@ -14,10 +14,11 @@ GLMakie.activate!(inline=false)
 
 ##
 
-
 # create GLMakie plot
 f = Figure()
 ax = Axis(f[1, 1])
+
+deregister_interaction!(ax, :rectanglezoom)
 limits!(ax, 0, 10, -4, 4)
 
 display(f)
@@ -45,18 +46,4 @@ app.log_start(gyro_cb, points, duration)
 println(cb)
 
 ##
-
-
-# fps = 60
-# nframes = 120
-
-# for i = 1:nframes
-#     new_point = randn(1)
-#     points[] = push!(points[], [i sin(i)])
-#     sleep(1 / fps) # refreshes the display!
-# end
-
-##
-# clear plot
-# delete!(ax, line_plot)
 empty!(ax)
