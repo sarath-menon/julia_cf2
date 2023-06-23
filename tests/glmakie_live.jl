dbd971afd8c3df36c3d782067f2af4ca1b86162c
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-06-23 09:29:50 +0200

    live plot scrolling axis works

--- tests/glmakie_live.jl
+++ tests/glmakie_live.jl
@@ -4,7 +4,9 @@ GLMakie.activate!(inline=false)
 
 f = Figure()
 ax = Axis(f[1, 1])
-limits!(ax, 0, 10, -4, 4)
+limits!(ax, 0, 120, -4, 4)
+
+x_range = 10
 
 display(f)
 
@@ -15,11 +17,28 @@ line_plot = lines!(ax, points)
 fps = 60
 nframes = 120
 
+on(points) do point
+    x_rlimit = last(points.val)[1][1]
+    println("Latest point is:", x_rlimit)
+
+    if x_rlimit >= x_range
+        x_llimit = x_rlimit - x_range
+    else
+        x_llimit = 0
+    end
+
+    limits!(ax, x_llimit, x_rlimit, -4, 4)
+end
+
+
 for i = 1:nframes
     new_point = randn(1)
     points[] = push!(points[], [i sin(i)])
     sleep(1 / fps) # refreshes the display!
 end
+
+
+
 
 ##
 # clear plot
