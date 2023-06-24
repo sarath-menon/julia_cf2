fc6d49460945c75a04d34ee9c0daf53bc102b996
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-06-24 09:56:41 +0200

    avoid overly specific types

--- src/julia_cf2.jl
+++ src/julia_cf2.jl
@@ -2,13 +2,13 @@ module julia_cf2
 using Revise
 
 
-rad_per_sample_to_hz(x::Float64) = x / (2 * π)
+rad_per_sample_to_hz(x::Real) = x / (2 * π)
 
-hz_to_rad_per_sample(x::Float64) = 2 * π * x
+hz_to_rad_per_sample(x::Real) = 2 * π * x
 
-dB_to_normal(x::Float64) = 10.0^(x / 20)
+dB_to_normal(x::Real) = 10.0^(x / 20)
 
-normal_to_dB(x::Float64) = 20 * log10.(x)
+normal_to_dB(x::Real) = 20 * log10.(x)
 
 
 end # module julia_cf2
