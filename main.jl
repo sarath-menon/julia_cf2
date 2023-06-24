56ce26bb4730f3a1657b599274135f6176f02fd8
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-06-24 11:38:34 +0200

    working

--- main.jl
+++ main.jl
@@ -2,7 +2,7 @@
 import Pkg;
 Pkg.activate(@__DIR__);
 Pkg.instantiate();
-
+##
 using Revise
 import julia_cf2
 using PyCall
@@ -12,21 +12,8 @@ using GLMakie
 
 GLMakie.activate!(inline=false)
 ##
-include("gui.jl")
 
-gui = gui_init()
-
-
-
-##
-
-# create circular buffer to hold gyro data
-const gyro_cb_len::Integer = 5
-gyro_cb = CircularBuffer{Array{Float64,2}}(gyro_cb_len)
-
-##
-include("tasks.jl")
-
+include("src/tasks/tasks.jl")
 
 schedule(cfread_task)
 schedule(main_task)
