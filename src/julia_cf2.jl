f47689d31f82fbfdcb02e0fb2858f9fa4bafedec
Sarath S Menon <sarathmenon.downey@gmail.com>
2023-06-24 09:10:13 +0200

    use shorthand notation for functions

--- src/julia_cf2.jl
+++ src/julia_cf2.jl
@@ -2,28 +2,13 @@ module julia_cf2
 using Revise
 
 
-function rad_per_sample_to_hz(val_rad)
-    val_hz = val_rad / (2 * π)
-    return val_hz
-end
+rad_per_sample_to_hz(x::Float64) = x / (2 * π)
 
-function hz_to_rad_per_sample(val_hz)
-    val_rad_per_sec = 2 * π * val_hz
-    return val_rad_per_sec
-end
+hz_to_rad_per_sample(x::Float64) = 2 * π * x
 
-function dB_to_normal(val)
-    return 10.0^(val / 20)
-end
+dB_to_normal(x::Float64) = 10.0^(x / 20)
 
-function normal_to_dB(val)
-    return 20 * log10.(val)
-end
-
-
-function selva()
-    print("ok")
-end
+normal_to_dB(x::Float64) = 20 * log10.(x)
 
 
 end # module julia_cf2
